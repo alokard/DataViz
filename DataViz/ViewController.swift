0285748156d7ed276d7256e426d692de8366eb18
Eugene Tulusha <tulusheg@gmail.com>
2018-02-19 17:30:13 +0200

    Initial Commit

---
+++ DataViz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataViz
+//
+//  Created by Eugene Tulusha on 2/19/18.
+//  Copyright © 2018 Eugene Tulusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
